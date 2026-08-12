b0a5d8150ae598bdd28f6b230b5e5ef5b07baa3b
sofia <sofiagomezsanchez6@gmail.com>
2026-08-11 23:47:53 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "http://sofiagomezsanchez6-estrellas.github.io", //"https://djsiddz.github.io",
-base: "/sofiagomezsanchez6-estrellas.github.io",
+  base: "/space-ahead",
   integrations: [
     swup({
       theme: ["overlay", { direction: "to-top" }],
