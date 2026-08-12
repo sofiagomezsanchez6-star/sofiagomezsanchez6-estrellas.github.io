565f55ebf325579fd4730b655662529990380ddc
sofia <sofiagomezsanchez6@gmail.com>
2026-08-12 05:06:48 +0000

    fix: ajustar base path en config y workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://sofiagomezsanchez6-estrellas.github.io", //"https://djsiddz.github.io",
-  base: "/space-ahead",
+  site: "https://sofiagomezsanchez6-star.github.io",
+  base: "/sofiagomezsanchez6-estrellas.github.io",
+
   integrations: [
     swup({
       theme: ["overlay", { direction: "to-top" }],
